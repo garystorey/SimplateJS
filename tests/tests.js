31242940eeb8b416903ac89b7c1db66915147795
Gary Storey <garystorey@users.noreply.github.com>
2015-03-05 17:45:24 -0500

    Update and rename tests to tests/tests.js

---
+++ tests/tests.js
@@ -0,0 +1,28 @@
+
+QUnit.test( "Testing SimplateJS: ", function( assert ) {
+
+  var testString = '<div class="{{class}}"></div>';
+
+  var testObject = {"class" : "test1"};
+  var testObjectString = '<div class="test1"></div>';
+
+  var testArray = [ {"class" : "test1"}, {"class" : "test2"}  ];
+  var testArrayString = '<div class="test1"></div><div class="test2"></div>';
+
+
+  /* Has SimplateJS been initialized? */
+  assert.strictEqual( typeof simplate,'object' , " - Simplate is available." );
+
+  /* Does the SET/GET functions exist? */
+  assert.strictEqual( typeof simplate.set,'function' , " - SET is available." );
+  assert.strictEqual( typeof simplate.get,'function' , " - GET is available." );
+
+  /* Does the SET function work correctly? */
+  assert.strictEqual( simplate.set('div', testString ), true , " - SET adds template." );
+
+  /* Does the Get function work correctly? */
+  assert.strictEqual( simplate.get('div'), testString , " - GET (basic) returns template." );
+  assert.strictEqual( simplate.get('div', testObject), testObjectString , " - GET with data returns template correctly." );
+  assert.strictEqual( simplate.get('div', testArray), testArrayString , " - GET with data array returns template correctly." );
+
+});
