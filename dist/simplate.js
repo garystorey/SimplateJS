13294e40a728658e21f0b745812683e4d21024ca
Gary Storey <garystorey@users.noreply.github.com>
2015-03-17 14:53:41 -0400

    Added support for "get" by id.

--- dist/simplate.js
+++ dist/simplate.js
@@ -20,10 +20,14 @@
   }
 
   /************
-   * getTemplate ( string [, array ] );
+   * getTemplate ( string [, array/object ] );
   ************/
-  function getTemplate( name, data ) {
-    return ( data && parseTemplate( name, data )) || ( cache[ name ] || false );
+  function getTemplate( name , data ){
+    var tmpl =  (name.indexOf('#') === 0 ) ?
+                document.getElementById( name.replace('#','') ).innerText :
+                ( cache[name] || false );
+
+    return ( data && parseTemplate( tmpl, data ) ) || tmpl;
   }
 
   /************
@@ -34,26 +38,102 @@
   }
 
   /************
-   * parseTemplate ( string [, array ] );
+   * parseTemplate ( string [, array/object ] );
   ************/
-  function parseTemplate( name, data ) {
+  function parseTemplate( template, data ) {
     var i = 0, len, str='';
     if ( data.constructor === Array ) {
       len = data.length;
       for(; i < len; i++){
-        str += replaceValues( name, data[i] );
+        str += replaceValues( template, data[i] );
       }
     } else {
-      str = replaceValues( name, data );
+      str = replaceValues( template, data );
     }
     return str;
   }
 
+
   /************
    * replaceValues ( string , object );
   ************/
-  function replaceValues( name, data ) {
-    var template = getTemplate(name);
+  function replaceValues( template, data ) {
+    if ( template ){
+      return template.replace(/{{([a-z_]+[a-z0-9_]*)}}/gi, function( tag, val ) {
+        return data[ val ] ? data[ val ]  : '';
+      });
+    } else {
+      return '';
+    }
+  }
+
+  return {
+    get : getTemplate,
+    set : setTemplate,
+    has : hasTemplate
+  };
+
+}));
+(function ( root, factory ) {
+  if ( typeof define === "function" && define.amd ) {
+    define( factory );
+  } else if( typeof module === "object" && module.exports ) {
+    module.exports = ( root.simplate = factory() );
+  } else {
+    root.simplate = factory();
+  }
+}( this, function() {
+
+  'use strict';
+
+  var cache = [];  /* Stores templates */
+
+  /************
+   * setTemplate ( string, string );
+  ************/
+  function setTemplate( name, data ) {
+    if ( ! hasTemplate( name ) ) { cache[ name ] = data; return true; } else { return false; }
+  }
+
+  /************
+   * getTemplate ( string [, array/object ] );
+  ************/
+  function getTemplate( name , data ){
+    var tmpl =  (name.indexOf('#') === 0 ) ?
+                document.getElementById( name.replace('#','') ).innerText :
+                ( cache[name] || false );
+
+    return ( data && parseTemplate( tmpl, data ) ) || tmpl;
+  }
+
+  /************
+  * hasTemplate ( string );
+  ************/
+  function hasTemplate( name ) {
+    return !!( cache[ name ] );
+  }
+
+  /************
+   * parseTemplate ( string [, array/object ] );
+  ************/
+  function parseTemplate( template, data ) {
+    var i = 0, len, str='';
+    if ( data.constructor === Array ) {
+      len = data.length;
+      for(; i < len; i++){
+        str += replaceValues( template, data[i] );
+      }
+    } else {
+      str = replaceValues( template, data );
+    }
+    return str;
+  }
+
+
+  /************
+   * replaceValues ( string , object );
+  ************/
+  function replaceValues( template, data ) {
     if ( template ){
       return template.replace(/{{([a-z_]+[a-z0-9_]*)}}/gi, function( tag, val ) {
         return data[ val ] ? data[ val ]  : '';
