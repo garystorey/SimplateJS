a01efa75ed88af6f029082cd51469f848039ad2d
Gary Storey <garystorey655@gmail.com>
2015-09-04 00:39:28 -0400

    fixed invalid jscsrc file
    Removed all but two errors I couldn't find a way around.

--- src/simplate.js
+++ src/simplate.js
@@ -1,74 +1,84 @@
-(function ( root, factory ) {
+(function(root, factory) {
   'use strict';
-  if ( typeof define === 'function' && define.amd ) {
-    define( factory );
-  } else if( typeof module === 'object' && module.exports ) {
-    module.exports = ( root.simplate = factory() );
+
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = (root.simplate = factory());
   } else {
     root.simplate = factory();
   }
-}( this, function() {
+}(this, function() {
 
   'use strict';
 
-  var cache = [];  /* Stores templates */
+  var cache = []; /* Stores templates */
 
   /************
    * setTemplate ( string, string );
   ************/
-  function setTemplate( name, data ) {
-    if ( ! hasTemplate( name ) ) { cache[ name ] = data; return data; } else { return false; }
+  function setTemplate(name, data) {
+    if (!hasTemplate(name)) {
+      cache[name] = data; return data;
+    } else {
+      return false;
+    }
   }
 
   /************
    * getTemplate ( string [, array/object ] );
   ************/
-  function getTemplate( name , data ){
-    var tmpl =  (name.indexOf('#') === 0 ) ?
-                getDomElement( name ) :
-                ( cache[name] || false );
+  function getTemplate(name, data) {
+    var tmpl = (name.indexOf('#') === 0) ?
+                getDomElement(name) :
+                (cache[name] || false);
 
-    return ( data && parseTemplate( tmpl, data ) ) || tmpl;
+    return (data && parseTemplate(tmpl, data)) || tmpl;
   }
 
-  function getDomElement( name ) {
-    name = name.replace('#','') + '';
-    if ( document && name ) {
-      return document.getElementById( name ).innerText;
+  function getDomElement(name) {
+    name = name.replace('#', '') + '';
+    if (document && name) {
+      return document.getElementById(name).innerText;
     }
+
     return false;
   }
 
   /************
   * hasTemplate ( string );
   ************/
-  function hasTemplate( name ) {
-    return !!( cache[ name ] );
+  function hasTemplate(name) {
+    return !!(cache[name]);
   }
 
   /************
    * parseTemplate ( string [, array/object ] );
   ************/
-  function parseTemplate( template, data ) {
-    var i = 0, len, str='';
-    if ( data.constructor === Array ) {
+  function parseTemplate(template, data) {
+    var i = 0,
+        len = 0,
+        str = '';
+
+    if (data.constructor === Array) {
       len = data.length;
-      for(; i < len; i++){
-        str += replaceValues( template, data[i] );
+      for (; i < len; i++) {
+        str += replaceValues(template, data[i]);
       }
     } else {
-      str = replaceValues( template, data );
+      str = replaceValues(template, data);
     }
+
     return str;
   }
 
   /************
    * replaceValues ( string , object );
   ************/
-  function replaceValues( template, data ) {
-    if ( template ){
-      return template.replace(/{{([a-z_]+[a-z0-9_]*)}}/gi, function( tag, val ) {
-        return  data[ val ] ? data[ val ]  : '{{'+val+'}}';
+  function replaceValues(template, data) {
+    if (template) {
+      return template.replace(/{{([a-z_]+[a-z0-9_]*)}}/gi, function(ag, val) {
+        return data[val] ? data[val] : '{{' + val + '}}';
       });
     } else {
       return '';
@@ -76,9 +86,9 @@
   }
 
   return {
-    get : getTemplate,
-    set : setTemplate,
-    has : hasTemplate
+    get:getTemplate,
+    set:setTemplate,
+    has:hasTemplate,
   };
 
 }));
